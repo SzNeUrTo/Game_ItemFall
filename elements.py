371661255c4c46a5ac0897c79565babca754ebe5
Supanut Apikulvanich <SugarSeeker@Supanuts-MacBook-Air.local>
2014-12-16 12:55:53 +0700

    getPointelements

--- elements.py
+++ elements.py
@@ -10,6 +10,11 @@ class Item(object):
         self.initValue(windowSizeX, selectfile)
 
     def initValue(self, windowSizeX, selectfile) :
+        self.point = selectfile.split('_')
+        if self.point[1] == 'Minus':
+            self.point = -self.point[0]
+        else:
+            self.point = self.point[0]
         self.positionX = random() * (windowSizeX - Item.IMAGE_SIZE)
         self.positionY = -Item.IMAGE_SIZE # + random because item not fall in same time
         self.speedFall = 5
@@ -33,6 +38,9 @@ class Item(object):
 
     def getImageSize(self):
         return Item.IMAGE_SIZE
+
+    def getPoint(self):
+        return self.point
 
     def render(self, surface):
         self.itemRect = pygame.Rect(self.positionX, self.positionY, Item.IMAGE_SIZE, Item.IMAGE_SIZE)
